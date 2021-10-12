acefc50d585e02574ed7c0c4c981b9a6ea4c037c
Kerem <o.keremorenli@gmail.com>
2021-10-12 17:39:32 +0300

    ilk görev tamam beybi

--- firstPlugin/src/taskpane/taskpane.js
+++ firstPlugin/src/taskpane/taskpane.js
@@ -17,8 +17,22 @@ Office.onReady((info) => {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("calculate").onclick = calculate;
+    document.getElementById("reload").onclick = reload;
   }
 });
+
+// ACTIVITY TYPE DATA
+var ActivityTypeDictionary = {
+  "34": "Faz-1 Betonu Dökülmesi",
+  "35": "Faz-2 Betonu Dökülmesi",
+  "08": "Rayların Hatta Dağıtılması",
+  "38": "Panelrayların Serilmesi",
+  "41": "Grout Dökülmesi",
+  "39": "Panelrayların Kot ve Eksen Ayarının Yapılması",
+  "09": "Rayların Hatta Montajı",
+  "43": "Plinth Beton Dökümü"
+};
+
 
 // FUNCTIONS
 function populateList(listId, arr) {
@@ -39,8 +53,6 @@ function findIndex(arr, value) {
   return -1
 }
 
-// console.log(findIndex(["a", "b", "c"], "f"));
-
 function findActivityTypes(columns_arr, data) {
   let key_column_name = "WBSADI"; // ! bu kolon adına göre hareket edecek, önemli.
   let index = findIndex(columns_arr, key_column_name);
@@ -50,12 +62,16 @@ function findActivityTypes(columns_arr, data) {
     if (wbsValue == key_column_name) {
       continue; // data'nın ilk satırı kolon başlıklarıysa elemek için.
     }
-    var activityCode = wbsValue.slice(wbsValue.length-2, wbsValue.length);
+    var activityCode = wbsValue.slice(wbsValue.length - 2, wbsValue.length);
     if (findIndex(activityTypes, activityCode) == -1) {
       activityTypes.push(activityCode);
-    } 
+    }
   }
   return activityTypes;
+}
+
+function getTheLastX(str, x) {
+   return str.slice(str.length - x, str.length);
 }
 
 // CALCULATE BUTTON
@@ -75,14 +91,46 @@ function calculate() {
         var countOfRows = tableRange.rowCount;
         document.getElementById("number_of_rows").innerHTML = countOfRows;
 
+        var columns = columnsRow.values[0];
         document.getElementById("columns").innerHTML = "";
-        populateList("columns", columnsRow.values[0]);
+        populateList("columns", columns);
 
-        var activityTypes = findActivityTypes(columnsRow.values[0], tableRange.values)
+        var data = tableRange.values;
+        var activityTypes = findActivityTypes(columnsRow.values[0], data)
+        var tableHTML = document.getElementById("results_table");
+        var x = 1;
         for (let i of activityTypes) {
-             
+            let row = tableHTML.insertRow(x);
+            row.insertCell(0).innerHTML = i;
+            row.insertCell(1).innerHTML = ActivityTypeDictionary[i];
+            
+            // * realized quantity calculation
+            let quantity = 0;
+            for (let d of data) {
+              // ! kolon isimleri önemli
+              if (getTheLastX(d[findIndex(columns,"WBSADI")],2) == i) {
+                quantity = quantity + Number(d[findIndex(columns,"ILERLEME")]);
+              }
+            };
+            row.insertCell(2).innerHTML = quantity;
+            
+            // * working hours calculation
+            let workingHours = 0;
+            for (let d of data) {
+              // ! kolon isimleri önemli
+              if (getTheLastX(d[findIndex(columns,"WBSADI")],2) == i
+               && d[findIndex(columns,"ISDETAYI")] == "Çalışma"
+              ) {
+                workingHours = workingHours + Number(d[findIndex(columns,"CALISMASAATI")]);
+              }
+            };
+            row.insertCell(3).innerHTML = workingHours;
+            
+            row.insertCell(4).innerHTML = Math.round(quantity / workingHours * 10);
+
+            x++;
         };
-        
+
       }
     ).then(context.sync);
   })
@@ -100,3 +148,21 @@ function calculate() {
 // var cell2 = row.insertCell(1);
 // cell1.innerHTML = "new cell";
 // cell2.innerHTML = "new cell";
+
+
+// CALCULATE BUTTON
+function reload() {
+  Excel.run(function (context) {
+
+    // eslint-disable-next-line no-undef
+    location.reload();
+
+    return context.sync()
+  })
+    .catch(function (error) {
+      console.log("Error: " + error);
+      // if (error instanceof OfficeExtension.Error) {
+      //     console.log("Debug info: " + JSON.stringify(error.debugInfo));
+      // }
+    });
+}
